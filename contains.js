f3ac31e74a3181b463ea425843ca69185d80358d
jakemckenzie <jake314@uw.edu>
2018-07-07 21:51:36 -0700

    off by one error...this was tricky

--- contains.js
+++ contains.js
@@ -1,9 +1,12 @@
 function contains(a, b) {
-    for (var i = 0, j = 0; i < a.length; i++){
-        if (a[i] == b[j]){
-            if (++j >= b.length){
-                return true
-            }
+    for (var i = 0, j = 0; i < a.length; i++) {
+        if (a[i] == b[j]) {
+            if (++j >= b.length)
+                return true;
+        }
+        else {
+            i -= j;
+            j = 0;
         }
     }
     return false;

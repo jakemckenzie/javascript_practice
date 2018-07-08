5125069c32b45fd4faf809d5f49b1bed72d4ff3c
jakemckenzie <jake314@uw.edu>
2018-07-07 21:48:42 -0700

    read the question jake...it wasn't so simple...or it was but read the damn question

--- contains.js
+++ contains.js
@@ -1,16 +1,10 @@
-function contains(set1,set2) {
-     for (var i = 0; i < set1.length - set2.length; i++)
-         if (!matches(set1, i, set2)){
-             return false;
-         }
-     return true;
-}
-
-function matches(int[] set1, int off, int[] set2) {
-     for (int j = 0; j < set2.length; j++) {
-         if (set1[off + j] != set2[j]) {
-             return false;
-         }
-     }
-     return true;
+function contains(a, b) {
+    for (var i = 0, j = 0; i < a.length; i++){
+        if (a[i] == b[j]){
+            if (++j >= b.length){
+                return true
+            }
+        }
+    }
+    return false;
 }
